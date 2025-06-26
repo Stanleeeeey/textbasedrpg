9e5cc75cc43a2bb4d2e9bebf6004b1b1fcb33aa9
Stanislaw Kawulok <dskybus@gmail.com>
2025-06-26 20:53:07 +0200

    quick change of libraries to pynput

--- modules/keyboard.py
+++ modules/keyboard.py
@@ -1,11 +1,19 @@
-import keyboard
 
+import pynput
+from pynput import keyboard
 
 
 class Keyboard:
     def __init__(self,):
         self.is_pressed = False
         self.already_used = False
+
+        listener = keyboard.Listener(
+            on_press=self.set_good,
+            on_release=self.set_bad
+        )
+
+        listener.start()
 
     def is_enter_just_pressed(self):
 
@@ -14,11 +22,23 @@ class Keyboard:
             return True
         return False
     
+    def set_good(self, key):
+        if key == keyboard.Key.enter:
+            self.is_pressed = True
+            
+
+    def set_bad(self, key):
+        if key == keyboard.Key.enter:
+            self.is_pressed = False
+            self.already_used = False
+    
     def update(self):
+        return
+        '''
         if not keyboard.is_pressed("enter"):
             self.already_used = False
             self.is_pressed = False
         else:
             self.is_pressed = True
-
+``      '''
 
